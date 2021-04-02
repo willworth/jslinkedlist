ab6b629eec495d4d99386398c3f8bfd67d3a954e
Will <will.worth@gmail.com>
2021-04-02 11:53:26 +0200

    getByIndex

--- LinkedList.js
+++ LinkedList.js
@@ -15,6 +15,19 @@ class LinkedList {
 
   //A common pitfall for novice developers is to put a comma between class methods,
   // which would result in a syntax error.
+
+  //slower than with an array...
+  getByIndex(index) {
+    // early return
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
 }
 
 // this could just be a js object but is a class here for consistency
@@ -25,5 +38,15 @@ class LinkedListNode {
     this.next = next;
   }
 }
+
+// Helper function
+LinkedList.fromValues = function (...values) {
+  const ll = new LinkedList();
+  // do it in reverse as we're inserting at beginning
+  for (let i = values.length - 1; i >= 0; i--) {
+    ll.insertAtHead(values[i]);
+  }
+  return ll;
+};
 
 module.exports = LinkedList;
--- LinkedList.test.js
+++ LinkedList.test.js
@@ -12,3 +12,35 @@ describe("#insertAtHead", () => {
     expect(ll.length).toBe(2);
   });
 });
+
+describe("#getByIndex", () => {
+  describe("with index < 0", () => {
+    test("it returns null", () => {
+      // helper so as to avoid other methods
+      const ll = LinkedList.fromValues(10, 20);
+
+      expect(ll.getByIndex(-1)).toBeNull();
+    });
+  });
+  describe("with index greater than list length", () => {
+    test("it returns null", () => {
+      const ll = LinkedList.fromValues(10, 20);
+
+      expect(ll.getByIndex(4)).toBeNull();
+    });
+  });
+  describe("with index 0", () => {
+    test("it returns the head", () => {
+      const ll = LinkedList.fromValues(10, 20);
+
+      expect(ll.getByIndex(0).value).toBe(10);
+    });
+  });
+  describe("with index in middle", () => {
+    test("it returns the element at that index", () => {
+      const ll = LinkedList.fromValues(10, 20, 30, 40);
+
+      expect(ll.getByIndex(2).value).toBe(30);
+    });
+  });
+});
